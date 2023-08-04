af0775ecf50e8d97fe302a21588957136f70a19c
SF-testUmnyasha <newashedelo@gmail.com>
2023-08-04 21:22:23 +0300

    Initial Commit

---
+++ KuponForOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KuponForOne
+//
+//  Created by Oleg Arnaut  on 04.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
